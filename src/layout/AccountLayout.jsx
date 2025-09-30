a1cc8b3b15196463280a518e57fa3fd46481cb9b
Vitaminazz <vitamina.twitu@gmail.com>
2025-09-29 23:08:33 -0300

    Correção do caminho do Footer no AccountLayout

--- src/layout/AccountLayout.jsx
+++ src/layout/AccountLayout.jsx
@@ -1,20 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import './AccountLayout.css'
-import Footer from '../Components/Footer/Footer';
+import { Outlet } from "react-router-dom";
+import "./AccountLayout.css";
+import Footer from "../components/Footer/Footer";
 
 export default function AccountLayout() {
   return (
     <div className="account-layout">
-      
-      {/* Conteúdo dinâmico das rotas */}
       <main className="account-content-area">
-        <Outlet /> {/* Aqui as páginas serão injetadas */}
+        <Outlet />
       </main>
-      
-     
       <Footer />
     </div>
   );
 }
-
-
