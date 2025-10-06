391d6cb4e7e5ced4e7239340c29a53113d5f0bd7
WillianSCorrea <willianwcorrea@gmail.com>
2025-10-06 14:31:55 -0300

    Add LoginPage component with LoginForm integration

---
+++ SabidosWeb/src/pages/Login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Login from '../../Components/auth/LoginForm';
+function LoginPage() {
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default LoginPage
