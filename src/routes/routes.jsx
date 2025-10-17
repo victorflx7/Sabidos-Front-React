36da8ba439c528ecb3f2d13a60df2321d6b06d5a
ihugow <orushugow@gmail.com>
2025-10-16 21:10:54 -0300

    feat: Instanciação do tailwind e correções na estrutura do projeto

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home";
 //import Agenda from "../pages/Agenda/Agenda";
-import Dashboard from "../pages/Dashboard/Dashboard";
+import Dashboard from "../pages/Dashboard";
 //import Perfil from "../pages/Perfil/Perfil";
 //import Pomodoro from "../pages/Pomodoro/Pomodoro";
 //import Resumo from "../pages/Resumo/Resumo";
 //import SobreNos from "../pages/SobreNos/SobreNos";
-import Cadastro from "../pages/Cadastro/Cadastro";
-import Login from "../pages/Login/Login";
+import Cadastro from "../pages/Cadastro";
+import Login from "../pages/Login";
 //import Flashcard from "../pages/Flashcard/Flashcard";
 import AccountLayout from "../layout/AccountLayout";
 
