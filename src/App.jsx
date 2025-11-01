ef423f657b4e9b2d9981071fc7e2e08e547959cd
WillianSCorrea <willianwcorrea@gmail.com>
2025-11-01 13:24:15 -0300

    nbsmnfsndf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes/routes";
+import AppRoutes from './routes/routes';
+import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContexts"; // ✅ IMPORTAR
+
 
 function App() {
   return (
