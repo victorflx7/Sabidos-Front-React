27e40e1308da0cf565e585a614e7e4561813112f
WillianSCorrea <willianwcorrea@gmail.com>
2025-11-21 15:39:14 -0300

    Correção bem aplicada Das chamadas para o backend do endpoint agenda

--- src/pages/Agenda/index.jsx
+++ src/pages/Agenda/index.jsx
@@ -1,7 +1,7 @@
 // pages/Agenda/AgendaPage.jsx
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContexts';
-import { EventoApi } from '../../services/EventoAPi.js';
+import { EventoApi } from '../../services/EventoApi.js';
 
 const AgendaPage = () => {
     const { currentUser, backendUser } = useAuth();
