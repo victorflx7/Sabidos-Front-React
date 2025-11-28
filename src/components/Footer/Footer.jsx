c6422c7613b11db4f427525e3371d484a8f3d575
Vitaminazz <vitamina.twitu@gmail.com>
2025-11-28 00:19:52 -0300

    Adição do Footer nas paginas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,21 @@
-import './Footer.css'
-
-
-   
-function Footer() {
-
+export default function Footer() {
   return (
-        <>
-            <footer >
-            <div className="Redes">
-                    <img src="icon/instagram.svg" alt="Logo_Instagram" className="LOGREDES"/>
-                     <img src="icon/X.svg" alt="Logo_Twitter_X" className="LOGREDES" />
-                    <img src="icon/github.svg" alt="Logo_GitHub" className="LOGREDES" />
-                 </div>
+    <footer className="w-full h-28 bg-[#292535] flex flex-col justify-evenly items-center items-center">
+      <div className="flex justify-evenly w-72">
+        <a href="https://www.instagram.com/jeniosiloucos/" target="_blank"><img src="IconesSVG/instagram.svg" alt="Instagram" className="w-8 h-8" /></a>
+        <a href="https://linktr.ee/Sabidos.d" target="_blank"><img src="IconesSVG/linkTree.svg" alt="Linktree" className="w-8 h-8" /></a>
+        <a href="mailto:sabidos.dev@gmail.com"><img src="IconesSVG/emailContato.svg" alt="Email de Contato" className="w-9 h-9" /></a>
+      </div>
 
-                 <div className="Centrado">
-                    
-                    <p id="pfoo" className="texto Centrado">
-                        © 2025 Sabidos. Todos os direitos reservados.
-                    </p>
-                </div>
-                <div className="List">
-                </div>
-            </footer>
-        </>
-  )
+      <div className="flex flex-col justify-center items-center text-center">
+        <p className="text-[#EAEAEA] font-semibold text-[13px]">
+          © 2025 Sabidos. Todos os direitos reservados.
+        </p>
+      </div>
+
+      <div className="text-[#b9b9b9] font-semibold text-[12px] flex space-x-4">
+        <span>De alunos, para alunos, crie, com o SabidoS²</span>
+      </div>
+    </footer>
+  );
 }
-
-export default Footer
